bf9a13dbfe97b4608ac19d49d925f77bb82d8b35
WlakerDevin12 <walkerdevin176@gmail.com>
2025-06-26 15:11:41 +0530

    Create con.py

---
+++ docs/con.py
@@ -0,0 +1,14 @@
+project = 'Trek GPS Update'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_favicon = '_static/favicon.png'
+html_js_files = [
+    'chatbot.js',
+]
